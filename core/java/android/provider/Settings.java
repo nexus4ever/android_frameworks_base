815d01911fc627bdb8cb03c1446a8b59374f7ffe
xyyx <xyyx@mail.ru>
2016-08-30 21:57:16 +0800

    volume rocker: volume key cursor control (1/2)
    Change-Id: I71d05b0f2ae3b9dc2239fde1e4ab790901aba804

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3561,6 +3561,15 @@ public final class Settings {
          */
 
         /**
+         * Volume keys control cursor in text fields (default is 1)
+         * 0 - Disabled
+         * 1 - Volume up/down moves cursor left/right
+         * 2 - Volume up/down moves cursor right/left
+         * @hide
+         */
+        public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+        /**
          * Volume rocker wake
          * @hide
          */
