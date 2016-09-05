11544f46c7a28b9c62a942073c60c2c9aa66bd07
Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
2016-09-05 16:11:59 +0900

    Hide QS customizer correctly
    When users open and close QS customizer quickly, QSCustomizer#hide()
    is not called because QSCustomizer#isCustomizing() returns false.
    The isCustomizing() becomes true when the expand animation ends.
    The hide() should be called even though the animation is ongoing.
    
    Bug: 30545089
    Test: manual - open and close QS customizer quickly
    
    Change-Id: Ic483addfb6ae9da31c997fec7778e5acd718c353
    
    Signed-off-by: xyyx <xyyx@mail.ru>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -258,7 +258,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
     }
 
     public void onCollapse() {
-        if (mCustomizePanel != null && mCustomizePanel.isCustomizing()) {
+        if (mCustomizePanel != null && mCustomizePanel.isShown()) {
             mCustomizePanel.hide(mCustomizePanel.getWidth() / 2, mCustomizePanel.getHeight() / 2);
         }
     }
@@ -493,7 +493,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
     }
 
     public void closeDetail() {
-        if (mCustomizePanel != null && mCustomizePanel.isCustomizing()) {
+        if (mCustomizePanel != null && mCustomizePanel.isShown()) {
             // Treat this as a detail panel for now, to make things easy.
             mCustomizePanel.hide(mCustomizePanel.getWidth() / 2, mCustomizePanel.getHeight() / 2);
             return;
