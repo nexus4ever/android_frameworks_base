7adb310478c548442665cee02dbeb486ccc52435
spezi77 <spezi77@gmx.com>
2016-09-11 20:57:17 +0200

    Core: Use ro.nexus4ever.version instead of ro.purenexus.version
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -50,8 +50,8 @@ public class Build {
     /** The name of the underlying board, like "goldfish". */
     public static final String BOARD = getString("ro.product.board");
 
-    /** The build PureNexus version */
-    public static final String NEXUS = getString("ro.purenexus.version");
+    /** The build nexus4ever version */
+    public static final String NEXUS = getString("ro.nexus4ever.version");
 
     /**
      * The name of the instruction set (CPU type + ABI convention) of native code.
