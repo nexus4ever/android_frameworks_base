5285053466c67431ee183e85fb6030b478078d5a
GwonHyeok <kh4975@naver.com>
2016-10-08 06:11:43 +0200

    FW_Base - XuiMod: Toast Animations [1/2]
    We already had these anim files:
    
    Change-Id: Ic4d1b573b8ed5c657e8e9b226c28944d6a0895ec
    Signed-off-by: STELIX <ssspinni@gmail.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4267,6 +4267,13 @@ public final class Settings {
         public static final String CALL_FLIP_ACTION_KEY = "call_flip_action_key";
 
         /**
+         * Toast animations
+         *
+         * @hide
+         */
+        public static final String TOAST_ANIMATION = "toast_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
