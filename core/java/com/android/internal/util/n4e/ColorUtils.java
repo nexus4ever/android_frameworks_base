7f2895e8da8e5616c917436047fbcd0eb9bacdf7
spezi77 <spezi77@gmx.com>
2016-11-14 22:43:07 +0100

    Nexusify [3/4].
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/com/android/internal/util/n4e/ColorUtils.java
+++ core/java/com/android/internal/util/n4e/ColorUtils.java
@@ -13,7 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package com.android.internal.util.nitrogen;
+package com.android.internal.util.n4e;
 
 import android.content.Context;
 import android.graphics.Bitmap;
